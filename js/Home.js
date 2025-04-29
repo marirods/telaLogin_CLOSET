3cf7f46fec30a39f897d1fec5065c2793f291060
Mariane Rodrigues <161612756+marirods@users.noreply.github.com>
2025-04-29 08:26:34 -0300

    Update Home.js

--- js/Home.js
+++ js/Home.js
@@ -96,7 +96,7 @@ async function dadosPublicacao(publicacao){
     
 }
 
-
+//
 async function getUsersbyId (id){
     const url = `https://back-spider.vercel.app/user/pesquisarUser/${id}`
     const response = await fetch(url)
